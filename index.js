502244627a61150018fcd97a761d0c936e38c835
bardhosmani <bardhiosmani01@gmail.com>
2021-06-12 14:08:22 +0200

    Fixed the issue

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 console.log("Kajtazi");
+console.log('Bardhi');
 const _ = require('lodash');
 
 app.get('/', (req, res) => {
